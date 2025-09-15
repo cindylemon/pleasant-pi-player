51a3a1185e265728296a647a2a55fa9a0dbf40de
cindy <cindy.wu23236@gmail.com>
2025-09-14 17:41:13 -0700

    everything works now

--- code/main.py
+++ code/main.py
@@ -1,8 +1,9 @@
 #import pygame as p
 import speech_recognition as sr
-# from pydub import AudioSegment
-# from pydub.playback import _play_with_simpleaudio as play
 import os
+import pvporcupine
+from pvrecorder import PvRecorder
+
 # Add ffmpeg to PATH before importing pydub
 os.environ["PATH"] += os.pathsep + r"C:\ffmpeg\bin"
 
@@ -27,31 +28,69 @@ audio_files = {
     "art of war 13":"audio/art_of_war_chinese_1506_librivox/artofwar_13_sun_64kb.mp3"
 }
 
-with sr.Microphone() as source:
-    print("listening..")
+keywords=["bumblebee", "jarvis"]
 
-    recognizer.adjust_for_ambient_noise(source)
-    audio = recognizer.listen(source)
+porcupine = pvporcupine.create(
+        access_key="REMOVED_API_KEY",
+        keywords = keywords)
+recorder = PvRecorder(device_index=-1, frame_length=porcupine.frame_length)
 
+
+
+
+def wakeWord():
+    print("running")
     try:
-        text = recognizer.recognize_google(audio).lower()
-        print(text)
-        audio_to_play = None
-
-        for title, audio_file in audio_files.items():
-            if title in text:
-                print("audio found, playing!")
-                audio_to_play = AudioSegment.from_mp3(audio_file)
+        recorder.start()
+        print("porcupine started")
+        while True:
+            keyword_index = porcupine.process(recorder.read())
+            if keyword_index >= 0:
+                print(f"deteced {keywords[keyword_index]}")
                 break
         
-        if audio_to_play:
-            play(audio_to_play)
-            print("playing")
-        else:
-            print("couldn't find matching audio")
+    except KeyboardInterrupt:
+        recorder.stop()
+    finally:
+        porcupine.delete()
+        recorder.delete()
 
-    except Exception as e:
-        print("something went wrong! Error:", e)
+
+
+
+def audioSearch():
+    while True:
+        wakeWord()
+        with sr.Microphone() as source:
+            recognizer.adjust_for_ambient_noise(source)
+            audio = recognizer.listen(source)
+            print("listening..")
+
+            try:
+                text = recognizer.recognize_google(audio).lower()
+                print(text)
+                if "exit" in text:
+                    print("exiting application")
+                    break
+                audio_to_play = None
+
+                for title, audio_file in audio_files.items():
+                    if title in text:
+                        print("audio found, playing!")
+                        audio_to_play = AudioSegment.from_mp3(audio_file)
+                        break
+                
+                if audio_to_play:
+                    play(audio_to_play)
+                    print("playing")
+                else:
+                    print("couldn't find matching audio")
+
+            except Exception as e:
+                print("something went wrong! Error:", e)
+
+print("hello?")
+audioSearch()
 
 
 
